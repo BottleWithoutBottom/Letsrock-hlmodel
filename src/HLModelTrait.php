24c471f4a1bf4a50d687f1440a04b73e9ccba4d8
Titov Leonid <shredder139@gmail.com>
2021-07-22 14:00:31 +0700

    *up: timeHelper method added

--- src/HLModelTrait.php
+++ src/HLModelTrait.php
@@ -14,4 +14,10 @@ trait HLModelTrait
     {
         return ['UF_ACTIVE' => 1];
     }
+
+    public static function getTimeCompatible($timeInSeconds)
+    {
+        global $DB;
+        return date($DB->DateFormatToPHP(\CSite::GetDateFormat("FULL")), $timeInSeconds);
+    }
 }
